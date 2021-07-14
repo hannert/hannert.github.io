c67863d852eb3427e44206c48e96aad5eaf1bbc0
hannert <79167052+hannert@users.noreply.github.com>
2021-07-14 17:48:02 -0400

    Update and Change
    General changes to the homepage, scaling elements updated from static.
    New weather webpage with working temperature.

--- js/typerwriter.js
+++ js/typerwriter.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded',function(event){
     // array with texts to type in typewriter
     var dataText = [ "Hi.", "My name is: <br /> Han Lin."];
+    var txtBox = document.getElementById("typewriterObj")
     // type one text in the typwriter
     // keeps calling itself until the text is finished
     function typeWriter(text, i, fnCallback) {
     // check if text isn't finished yet
         if (i < (text.length)) {
-            // add next character to h1
-        document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+            // add next character to top
+            txtBox.innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
     
             // wait for a while and call this function again for next character
             setTimeout(function() {
--- js/weatherAPI.js
+++ js/weatherAPI.js
@@ -1,3 +1,25 @@
 function changeText(text) {
-    let div = document.getElementById("")
+    let div = document.getElementById("temperature");
+    div.innerHTML = text;
 }
+
+let key = '915518152d67549280c3bbfc67ef45d2';
+
+const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=Brooklyn&units=imperial&appid=915518152d67549280c3bbfc67ef45d2';
+
+function getWeatherData(){
+    let request = new XMLHttpRequest();
+    let currentTime = new Date();
+    request.open('GET',weatherAPI,true);
+    console.log("Json Updated" + ", currentTime: " + currentTime.getHours() + ":" + currentTime.getMinutes());
+    request.send();
+    request.onload = function(){
+    let weather = JSON.parse(this.response);
+    let weatherTemp = weather.main.temp;
+    changeText(weatherTemp);
+    }
+}
+
+// On document ready, call initial getWeatherData
+$(document).ready(console.log("Ready"), getWeatherData());
+setInterval(getWeatherData, 10000);
