917a59772a40c5d92a30bc139e50b3b1b31e245d
Han <79167052+hannert@users.noreply.github.com>
2024-12-02 16:07:02 -0500

    Testing Nameplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
+'use client'
+import { motion } from "motion/react";
 import Image from "next/image";
+import Testcomponent from "./components/testcomponent";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <motion.div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Testcomponent/>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+
+        <motion.div
+          initial={{scale:0}}
+          animate={{scale:1}}
+        >
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={400}
+            height={30}
+            priority
+          />          
+        </motion.div>
+
+        <motion.div
+          className="width=400 height=300 color=[red]">
+          Awesome
+        </motion.div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -48,6 +63,17 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+
+      <motion.div
+
+        style={{}}
+      >
+
+
+
+      </motion.div>
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -58,16 +84,17 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/icons8-linkedin.svg"
             alt="File icon"
             width={16}
             height={16}
+            color="white"
           />
           Learn
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/hanqlin/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -80,22 +107,8 @@ export default function Home() {
           />
           Examples
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
       </footer>
-    </div>
+    </motion.div>
   );
 }
